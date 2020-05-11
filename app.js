dafc172b9415cf69e357c3e251f9134324ccf4f5
Githaka <githakamaru@gmail.com>
2020-05-11 11:43:43 +0300

    added handlebars and partials

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//connect to mongoose
+mongoose.connect('mongodb://localhost/brainstorm', {
+    useMongoClient: true
+})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
 
 //handlebars middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
 
 
 //Routing
