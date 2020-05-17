42609738cdcf55548d8545a5338a2cbeb97eab9b
Yahaya <yasholma1@gmail.com>
2020-05-17 17:20:51 +0100

    Fix the edit page error and implemented update functionality

--- app.js
+++ app.js
@@ -1,128 +1,156 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-
-
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
-
-
 
 //Map global promise - get rid of warning
 //mongoose.Promise = global.Promise;
 
 //connect to mongoose
-mongoose.connect('mongodb://localhost/brainstorm', {
-        useMongoClient: true,
-        useUnifiedTopology: true
+mongoose
+    .connect("mongodb://localhost/brainstorm", {
+        // useMongoClient: true,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
     })
-    .then(() => console.log('MongoDB Connected...'))
+    .then(() => console.log("MongoDB Connected..."))
     .catch(err => console.log(err));
 
 //Handle Idea Model
-require('./models/Ideas');
-const Idea = mongoose.model('ideas');
-
+require("./models/Ideas");
+const Idea = mongoose.model("ideas");
 
 //handlebars middleware
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main",
+    }),
+);
+app.set("view engine", "handlebars");
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-app.use(bodyParser.json())
-
-
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    }),
+);
+app.use(bodyParser.json());
 
 //Routing
 
 // Index Route
-app.get('/', (req, res) => {
-    const title = 'Welcome One';
-    res.render('index', {
-        title: title
+app.get("/", (req, res) => {
+    const title = "Welcome One";
+    res.render("index", {
+        title: title,
     });
 });
 
 // About Route
-app.get('/about', (req, res) => {
-    res.render('about');
-})
+app.get("/about", (req, res) => {
+    res.render("about");
+});
 
 // Idea Index Page
-app.get('/ideas', (req, res) => {
-    Idea.find({}).lean()
+app.get("/ideas", (req, res) => {
+    Idea.find({})
+        .lean()
         .sort({
-            date: 'desc'
+            date: "desc",
         })
         .then(ideas => {
-            res.render('ideas/index', {
-                ideas: ideas
+            res.render("ideas/index", {
+                ideas: ideas,
             });
         });
-
 });
 
 // Add Idea Form Route
-app.get('/ideas/add', (req, res) => {
-    res.render('ideas/add');
-})
+app.get("/ideas/add", (req, res) => {
+    res.render("ideas/add");
+});
 
 //Edit Idea form
 
-app.get('/ideas/edit/:id', (req, res) => {
+app.get("/ideas/edit/:id", (req, res) => {
     Idea.findOne({
-            _id: req.params.id
-        })
-        .then(idea => {
-            res.render('ideas/edit', {
-                idea: idea
-            });
-        });
+        _id: req.params.id,
+    }).then(idea => {
+        const { _id: id, title, details } = idea;
+        console.log(title);
 
+        res.render("ideas/edit", {
+            idea: { id, title, details },
+        });
+    });
 });
 
-
-
-
-
-// Process Form
-app.post('/ideas', (req, res) => {
+// Update Idea
+app.post("/ideas/update/:id", (req, res) => {
+    const id = req.params.id;
+    const { title, details } = req.body;
     let errors = [];
 
-    if (!req.body.title) {
+    if (!title) {
         errors.push({
-            text: 'Please add a title'
+            text: "Please add a title",
         });
     }
-    if (!req.body.details) {
+    if (!details) {
         errors.push({
-            text: 'Please add some details'
+            text: "Please add some details",
         });
     }
 
     if (errors.length > 0) {
-        res.render('ideas/add', {
+        res.render(`ideas/edit/${id}`, {
+            errors: errors,
+            title: title,
+            details: details,
+        });
+    } else {
+        Idea.findByIdAndUpdate({ _id: id }, { title, details })
+            .then(() => {
+                res.redirect("/ideas");
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+});
+
+// Process Form
+app.post("/ideas", (req, res) => {
+    let errors = [];
+
+    if (!req.body.title) {
+        errors.push({
+            text: "Please add a title",
+        });
+    }
+    if (!req.body.details) {
+        errors.push({
+            text: "Please add some details",
+        });
+    }
+
+    if (errors.length > 0) {
+        res.render("ideas/add", {
             errors: errors,
             title: req.body.title,
-            details: req.body.details
+            details: req.body.details,
         });
     } else {
         const newUser = {
             title: req.body.title,
-            details: req.body.details
-        }
-        new Idea(newUser)
-            .save()
-            .then(idea => {
-                res.redirect('/ideas');
-            })
+            details: req.body.details,
+        };
+        new Idea(newUser).save().then(idea => {
+            res.redirect("/ideas");
+        });
     }
 });
 
